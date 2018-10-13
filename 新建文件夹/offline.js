11b7011e7a9e8906e68ecb5640d96049daddee1d
mountaintouhou <932353094@qq.com>
2018-10-13 19:00:49 +0800

    11

---
+++ 新建文件夹/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1539427793,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/monhouse-sheet0.png",
+		"media/048-explosion01.ogg",
+		"media/052-cannon01.ogg",
+		"media/005-defeat01.ogg",
+		"media/001-battle.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
